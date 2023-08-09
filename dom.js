14c8a69d78a5e56fdf00d27bb80c9a36adbf269f
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-08-09 16:24:37 +0530

    que4_ans
    Updated for que 4.
    1- Item 3 was given green background.
    2- All list elements were made bold.

--- dom.js
+++ dom.js
@@ -11,9 +11,11 @@ var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000'
 
 var items = document.getElementsByClassName('list-group-item');
-console.log(items);
+// console.log(items);
+// for (var i = 0; i < items.length; i++) {
+//     items[i].style.fontWeight = 'bold';
+//     items[i].style.color = "green";
+// }
+items[2].style.backgroundColor = "green";
 for (var i = 0; i < items.length; i++) {
     items[i].style.fontWeight = 'bold';
-    items[i].style.color = "green";
-}
-
