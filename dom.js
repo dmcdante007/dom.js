f60a71e91e2a053f520084fb14068aefdd54512f
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-08-10 16:18:48 +0530

    que6_part1.js
    Used querySelector to grab 2 and 3 element and gave them properties.

--- dom.js
+++ dom.js
@@ -1,25 +1,5 @@
-// // console.log('Hello')
-// console.log(document.domain);
-// console.log(document.title);
-// console.log(document.URL);
-// console.log(document.doctype);
-// console.log(document.head);
-// console.log(document.body);
-console.log(document.all);
-var HeaderTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000'
+var sec_item = document.querySelector('.list-group-item:nth-child(2)');
+sec_item.style.backgroundColor = 'Green';
 
-// var items = document.getElementsByClassName('list-group-item');
-// // console.log(items);
-// // for (var i = 0; i < items.length; i++) {
-// //     items[i].style.fontWeight = 'bold';
-// //     items[i].style.color = "green";
-// // }
-// items[2].style.backgroundColor = "green";
-// for (var i = 0; i < items.length; i++) {
-//     items[i].style.fontWeight = 'bold';
-
-var li = document.getElementsByTagName('li');
-li[4].style.fontWeight = "bold";
-li[4].style.backgroundColor = "yellow";
+var third_item = document.querySelector('.list-group-item:nth-child(3)');
+third_item.style.color = 'White';
