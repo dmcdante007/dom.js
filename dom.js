b991c5b0a2429aec89b1102cdbeb9ac80158e738
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-08-10 15:57:53 +0530

    que5_ans.js
    Did the mentioned things:
    1- Added new Li item.
    2- Grabbed all elements using getelementbyTagname
    3- li[4] was made Bold and Yellow background.

--- dom.js
+++ dom.js
@@ -10,12 +10,16 @@ var HeaderTitle = document.getElementById('header-title');
 var header = document.getElementById('main-header');
 header.style.borderBottom = 'solid 3px #000'
 
-var items = document.getElementsByClassName('list-group-item');
-// console.log(items);
+// var items = document.getElementsByClassName('list-group-item');
+// // console.log(items);
+// // for (var i = 0; i < items.length; i++) {
+// //     items[i].style.fontWeight = 'bold';
+// //     items[i].style.color = "green";
+// // }
+// items[2].style.backgroundColor = "green";
 // for (var i = 0; i < items.length; i++) {
 //     items[i].style.fontWeight = 'bold';
-//     items[i].style.color = "green";
-// }
-items[2].style.backgroundColor = "green";
-for (var i = 0; i < items.length; i++) {
-    items[i].style.fontWeight = 'bold';
+
+var li = document.getElementsByTagName('li');
+li[4].style.fontWeight = "bold";
+li[4].style.backgroundColor = "yellow";
