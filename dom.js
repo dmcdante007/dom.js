8d4a52c82eafd13901b3249ee4b928f1e0400ac5
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-08-11 19:42:52 +0530

    que7_ans.js
    1- Added HEllO before item lister.
    2- Added HEllO bero before Item 1

--- dom.js
+++ dom.js
@@ -1,7 +1,31 @@
-var item = document.querySelectorAll('li');
-item[1].style.color = 'Red';
+var newDiv = document.createElement('div');
+newDiv.className = 'Hello 1';
+newDiv.id = 'Hello';
+newDiv.setAttribute('title', 'HEllO');
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-for (var i = 0; i < odd.length; i++) {
-    odd[i].style.backgroundColor = 'Green';
-}
+var newDivtext = document.createTextNode('HEllO'); //THis will be the text in the created div
+newDiv.appendChild(newDivtext);
+
+var container = document.querySelector('header .container');
+var h1 = document.querySelector('header  h1');
+
+container.insertBefore(newDiv, h1);
+
+
+var newDiv2 = document.createElement('div');
+newDiv2.className = 'List-group-item';
+newDiv2.id = 'item0';
+newDiv2.setAttribute('title', 'HEllO');
+
+var text1 = document.createTextNode('HEllO Bero');
+newDiv2.appendChild(text1);
+
+// console.log(newDiv2);
+
+
+var con = document.querySelector('ul');
+var h2 = document.querySelector('li');
+
+// console.log(con);
+// console.log(h2);
+con.insertBefore(newDiv2, h2);
