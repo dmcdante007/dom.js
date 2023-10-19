6207b17413f6179f4d240e018296c4689972bb9f
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-10-19 16:51:54 +0530

    Promises.all.js
    question no14:-  Making reg page dynamic

--- second_file.js
+++ second_file.js
@@ -1,1 +1,20 @@
-console.log('I am learing to push');
+function user() {
+    username: 'Swaraj'
+    lastTime : '19th Oct 23'
+}
+
+function updateLastUserActivityTime() {
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+           user.lastTime = new Date().getTime();
+           resolve(time); 
+        },1000)
+    })
+}
+
+userUpdatesPost(){
+    Promise.all([user, updateLastUserActivityTime]).then(([userResolves, updateLastUserActivityTimeResolves]) => {
+        console.log(userResolves);
+        console.log(updateLastUserActivityTimeResolves);
+    })
+}
