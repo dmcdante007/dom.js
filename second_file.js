f2965a935bf7f7f704d8e68b871c953d8ae5c120
SwarajMeshram <99945896+dmcdante007@users.noreply.github.com>
2023-10-26 00:13:49 +0530

    async_n_await.js
    Added getaCoke await to it

--- second_file.js
+++ second_file.js
@@ -1,20 +1,50 @@
-function user() {
-    username: 'Swaraj'
-    lastTime : '19th Oct 23'
-}
+console.log('person1 shows ticket');
+console.log('person2 shows ticket');
 
-function updateLastUserActivityTime() {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-           user.lastTime = new Date().getTime();
-           resolve(time); 
-        },1000)
-    })
-}
+const preMovie = async () => {
 
-userUpdatesPost(){
-    Promise.all([user, updateLastUserActivityTime]).then(([userResolves, updateLastUserActivityTimeResolves]) => {
-        console.log(userResolves);
-        console.log(updateLastUserActivityTimeResolves);
+    const person3PromiseToShowTicketWhenWifeArrives = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('ticket'), 3000);
+    });
+    const getPopcorn = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('popcorn'), 3000);
+    });
+
+    const addButter = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('butter'), 3000);
+    });
+
+    const addCoke = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('coke'), 3000);
     })
-}
+    let ticket = await person3PromiseToShowTicketWhenWifeArrives;
+
+    console.log(`got the ${ticket}`);
+    console.log(`Husband:we should go in now`);
+    console.log(`Wife: "i am hungry"`);
+
+    let popcorn = await getPopcorn;
+    console.log(`Husband: here is ${popcorn}`);
+    console.log(`Husband:we should go in now`);
+    console.log(`Wife: "I dont like popcorn without butter!"`);
+
+    let butter = await addButter;
+    console.log(`added ${butter}`);
+
+    console.log(`Husband:Anything else darling`);
+    console.log(`Wife: I want coke`)
+
+    let coke = await addCoke;
+    console.log(`added ${coke}`);
+    console.log(`Husband: Anything else baby?`)
+    console.log(`Wife: lets go we are going to miss the preivew`);
+    console.log(`Husband: thanks for the reminder *grin*`);
+
+
+    return ticket;
+
+};
+
+preMovie().then((t) => console.log(`person4 shows ${t}`));
+
+console.log('person4 shows ticket');
